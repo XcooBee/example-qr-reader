60a9b57954ffef2ee698aac3fe578224ea45f87e
Alex Melnyk <ask.melnyk@gmail.com>
2021-12-08 02:15:24 +0200

    Data params enclosed in SecurePayItem file.

--- src/models/SecurePayItem.ts
+++ src/models/SecurePayItem.ts
@@ -1,12 +1,45 @@
+/**
+ * URL data parameters.
+ */
+enum DataParams {
+  amount = '0-3',
+  tax = '0-5',
+  reference = '0-6',
+}
+
+/**
+ * Represents a secure pay item.
+ */
 export class SecurePayItem {
+  campaignRef: string;
+  formRef: string;
   amount: number;
   tax: number;
   reference: string;
 
-  constructor(amount: number, tax: number, reference: string) {
+  constructor(campaignRef: string, formRef: string, amount: number, tax: number, reference: string) {
+    this.campaignRef = campaignRef;
+    this.formRef = formRef;
     this.amount = amount;
     this.tax = tax;
     this.reference = reference;
+  }
+
+  /**
+   * Creates a new instance based on specified parameters.
+   *
+   * @param campaignRef - Campaign reference.
+   * @param formRef - Form reference.
+   * @param data - Decoded data.
+   */
+  static fromData(campaignRef: string, formRef: string, data: any): SecurePayItem {
+    return new SecurePayItem(
+      campaignRef,
+      formRef,
+      data[DataParams.amount],
+      data[DataParams.tax],
+      data[DataParams.reference],
+    );
   }
 
   toString(): String {
--- src/screens/Scanner.tsx
+++ src/screens/Scanner.tsx
@@ -11,12 +11,6 @@ enum UrlParams {
   DATA = 'd',
 }
 
-enum DataParams {
-  amount = '0-3',
-  tax = '0-5',
-  reference = '0-6',
-}
-
 const securePayUrl = 'https://app.xcoobee.net/securePay/';
 const path = new Path(`:${UrlParams.CAMPAIGN_REF}/:${UrlParams.FORM_REF}?:${UrlParams.DATA}`);
 
@@ -37,14 +31,14 @@ export const Scanner: React.FC = () => {
           const campaignRef = test[UrlParams.CAMPAIGN_REF];
           const formRef = test[UrlParams.FORM_REF];
 
+          // Data parameter is encoded to Base64
           const decodedJson = Buffer.from(test[UrlParams.DATA], 'base64').toString();
+          // Data contains list of items.
           const decodedData = JSON.parse(decodedJson) as Array<any>;
+          // Map all data items.
+          const secPayItems = decodedData.map((el) => SecurePayItem.fromData(campaignRef, formRef, el));
 
-          const secPayItems = decodedData.map((el) => new SecurePayItem(el[DataParams.amount],
-            el[DataParams.tax],
-            el[DataParams.reference]));
-
-          console.log('url', secPayItems);
+          console.log('Parsed Data', secPayItems);
 
           return true;
         }}
